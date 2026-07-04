a3052c26540cf141f04589b9d90a321f5235c355
NewbieXvwu <NewbieXvwu@qq.com>
2026-07-04 13:38:49 +0800

    fuck activate

--- Launchpad/Utilities/LaunchpadKeys.swift
+++ Launchpad/Utilities/LaunchpadKeys.swift
@@ -4,14 +4,14 @@ class LaunchpadKeys {
    private static let productKeys = ["BASIC", "PRO", "PREMIUM"]
 
    public static func isActivated(key: String) -> Bool {
-      return key == productKeys[0] || key == productKeys[1] || key == productKeys[2]
+      return true
    }
 
    public static func isPro(key: String) -> Bool {
-      return key == productKeys[1] || key == productKeys[2]
+      return true
    }
 
    public static func isPremium(key: String) -> Bool {
-      return key == productKeys[2]
+      return true
    }
 }
